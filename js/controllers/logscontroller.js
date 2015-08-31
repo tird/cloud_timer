fda0749280fc1b115fb1cd0d6c6e11d3ea25b52c
Evgeniy Stepanov <zhenyast@yandex.ru>
2015-08-31 13:38:33 +0300

    Update logscontroller.js

--- js/controllers/logscontroller.js
+++ js/controllers/logscontroller.js
@@ -2,10 +2,10 @@
 'use strict';
 angular.module('timerApp').controller('LogsController', ['$scope', 'TasksService', 'LogsService', '$route', function($scope, TasksService, LogsService, $route){
      if(LogsService.logs.length !==0){
-	     $scope.logs = LogsService.getLogs();
+	 $scope.logs = LogsService.getLogs();
      }
-	 $scope.deleteLog = function(index){
-	     LogsService.deleteLog(index);
+     $scope.deleteLog = function(index){
+         LogsService.deleteLog(index);
 	 }
      $scope.deleteAllLogs = function(){
          LogsService.deleteAllLogs();
